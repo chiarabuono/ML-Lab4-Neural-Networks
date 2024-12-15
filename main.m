bf2b1f9c6cb43dc7980f875c8401c5f637cc0981
Chiara <7687956@studenti.unige.it>
2024-12-15 07:26:02 +0100

    Task 1 and Task 2 written

---
+++ main.m
@@ -0,0 +1,60 @@
+clear all, close all
+%% TASK 1
+load glass_dataset
+
+x = glassInputs;
+t = glassTargets;
+
+% Choose a Training Function
+% For a list of all training functions type: help nntrain
+% 'trainlm' is usually fastest.
+% 'trainbr' takes longer but may be better for challenging problems.
+% 'trainscg' uses less memory. Suitable in low memory situations.
+trainFcn = 'trainscg';  % Scaled conjugate gradient backpropagation.
+
+% Create a Pattern Recognition Network
+hiddenLayerSize = 10;
+net = patternnet(hiddenLayerSize, trainFcn);
+
+% Setup Division of Data for Training, Validation, Testing
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
+
+% Train the Network
+[net,tr] = train(net,x,t);
+
+% Test the Network
+y = net(x);
+e = gsubtract(t,y);
+performance = perform(net,t,y);
+tind = vec2ind(t);
+yind = vec2ind(y);
+percentErrors = sum(tind ~= yind)/numel(tind);
+
+% View the Network
+view(net)
+
+% Plots
+figure, plotperform(tr)
+figure, plottrainstate(tr)
+figure, ploterrhist(e)
+figure, plotconfusion(t,y)
+figure, plotroc(t,y)
+
+%% TASK 2
+classNumber = 10; % number of class in MNIST database
+class1 = randi([1, classNumber]);
+class2 = randi([1,classNumber]);
+
+while class1 == class2
+    class2 = randi([1,classNumber]);
+end
+
+[TrSet, ClassSet] = loadMNIST(0,[class1,class2]);
+nh = 2; % size of hidden layers
+
+myAutoencoder = trainAutoencoder(TrSet', nh);
+myEncodedData = encode(myAutoencoder,TrSet');
+
+plotcl(myEncodedData', ClassSet); % Plot encoded data
